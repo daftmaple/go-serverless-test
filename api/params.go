91db946d52b3c7f117e89f4f2aadaf9c1c5fbb33
daftmaple <35413259+daftmaple@users.noreply.github.com>
2020-12-18 01:05:59 +1100

    Test prototype (#1)
    * Another test attempt
    
    * Added header and query params for testing

---
+++ api/params.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Handler ...
+func Handler(w http.ResponseWriter, r *http.Request) {
+	for name, headers := range r.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "Header %v: %v\n", name, h)
+		}
+	}
+
+	for name, queries := range r.URL.Query() {
+		for _, h := range queries {
+			fmt.Fprintf(w, "Query %v: %v\n", name, h)
+		}
+	}
+}
